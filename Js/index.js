1e716d0b7db3829317be13ecb0f2d851662c6dbb
pranavthakur404 <pranavufc@gmail.com>
2023-07-03 22:36:26 +0530

    Js Updation
    Updating js for hinding inspect element

--- Js/index.js
+++ Js/index.js
@@ -72,6 +72,24 @@ console.log(GeneratedNumber)
         }
     })
 
+// hiding inspect element 
+var body = document.querySelector("body");
+body.setAttribute("oncontextmenu","return false;")
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+}
+
 
 
 
